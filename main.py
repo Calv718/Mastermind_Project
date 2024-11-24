084bfe8d96a4305fd07fabde9eb06962efb64137
Calv718 <calvin9243@gmail.com>
2024-11-23 21:57:18 -0500

    Saving local changes

--- main.py
+++ main.py
@@ -5,11 +5,12 @@ import argparse
 from scsa import *
 from player import *
 from mastermind import *
-from LogicLegends_B1 import Baseline1
-from LogicLegends_B2 import Baseline2
-from LogicLegends_B3 import Baseline3
-from LogicLegends_B4 import Baseline4
-from LogicLegends_d3 import LogicLegends
+# from LogicLegends_B1 import Baseline1
+# from LogicLegends_B2 import Baseline2
+# from LogicLegends_B3 import Baseline3
+# from LogicLegends_B4 import Baseline4
+# from LogicLegends_d3 import LogicLegends
+from LogicLegends_d4 import LogicLegends
 
 parser = argparse.ArgumentParser(description="Play a game of Mastermind.")
 parser.add_argument("--board_length", nargs="?", type=int, required=True)
